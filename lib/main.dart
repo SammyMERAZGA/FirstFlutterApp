47b0b9de90b92d13e867f5dd83881245f58ad503
SammyMERAZGA <sammy.merazga@gmail.com>
2022-05-27 18:55:05 +0200

    Refactor Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:transparent_image/transparent_image.dart';
+import 'package:formation/recipeScreen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-        padding: const EdgeInsets.all(8),
-        child: Row(
-          children: [
-            Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Text("Pizza facile",
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                    ),
-                    Text("Par Smy", style: TextStyle(color: Colors.grey, fontSize: 16)),
-                  ],
-                )),
-            Icon(Icons.favorite, color: Colors.red),
-            Text("55")
-          ],
-        ));
 
-    Widget buttonSection = Container(
-      padding: const EdgeInsets.all(8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButtonColumn(Colors.blue, Icons.comment, 'COMMENT'),
-          _buildButtonColumn(Colors.blue, Icons.share, 'SHARE')
-        ],
-      ),
-    );
-
-    Widget descriptionSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        "Faire cuire dans une poêle les lardons et les champignons.\n\nDans un bol, verser la boîte de concentré de tomate, y ajouter un demi verre d'eau, ensuite mettre un carré de sucre (pour enlever l'acidité de la tomate) une pincée de sel, de poivre, et une pincée d'herbe de Provence. \n\nDérouler la pâte à pizza sur le lèche frite de votre four, piquer-le.\n\nAvec une cuillère à soupe, étaler délicatement la sauce tomate, ensuite y ajouter les lardons et les champignons bien dorer. Parsemer de fromage râpée.\n\nMettre au four à 220°, thermostat 7-8, pendant 20 min (ou lorsque le dessus de la pizza est doré).Faire cuire dans une poêle les lardons et les champignons.\n\nDans un bol, verser la boîte de concentré de tomate, y ajouter un demi verre d'eau, ensuite mettre un carré de sucre (pour enlever l'acidité de la tomate) une pincée de sel, de poivre, et une pincée d'herbe de Provence.\n\nDérouler la pâte à pizza sur le lèche frite de votre four, piquer-le.\n\nAvec une cuillère à soupe, étaler délicatement la sauce tomate, ensuite y ajouter les lardons et les champignons bien dorer. Parsemer de fromage râpée.\n\nMettre au four à 220°, thermostat 7-8, pendant 20 min (ou lorsque le dessus de la pizza est doré",
-        softWrap: true,
-      ),
-    );
 
     return MaterialApp(
       title: 'Formation Flutter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
-            title: Text("Mes recettes"),
-          ),
-          body: ListView(
-              children: [
-                FadeInImage.assetNetwork(
-                  placeholder: 'images/sharingan.gif',
-                  image: "https://thumbs.dreamstime.com/b/pizza-35669930.jpg",
-                  width: 600,
-                  height: 240,
-                  fit: BoxFit.cover,
-                ),
-                titleSection,
-                buttonSection,
-                descriptionSection,
-              ])
-      ),
-    );
-  }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          padding: const EdgeInsets.only(bottom: 8),
-          child: Icon(icon, color: color),
-        ),
-        Text(label,
-            style: TextStyle(
-                fontSize: 16, fontWeight: FontWeight.w400, color: color))
-      ],
+      home: RecipeScreen(),
     );
   }
 }
