be60c0656fc4f56b0b4753e00ea787ebcdefbe00
SammyMERAZGA <sammy.merazga@gmail.com>
2022-05-28 13:24:00 +0200

    Complex List Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:formation/recipe.dart';
+import 'package:formation/recipeListScreen.dart';
 import 'package:formation/recipeScreen.dart';
 
 void main() {
@@ -15,15 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: RecipeScreen(
-          recipe: Recipe(
-              title: "Pizza facile",
-              user: "Par Sammy Merazga",
-              imageUrl: "https://thumbs.dreamstime.com/b/pizza-35669930.jpg",
-              description:
-                  "Faire cuire dans une poêle les lardons et les champignons.\n\nDans un bol, verser la boîte de concentré de tomate, y ajouter un demi verre d'eau, ensuite mettre un carré de sucre (pour enlever l'acidité de la tomate) une pincée de sel, de poivre, et une pincée d'herbe de Provence. \n\nDérouler la pâte à pizza sur le lèche frite de votre four, piquer-le.\n\nAvec une cuillère à soupe, étaler délicatement la sauce tomate, ensuite y ajouter les lardons et les champignons bien dorer. Parsemer de fromage râpée.\n\nMettre au four à 220°, thermostat 7-8, pendant 20 min (ou lorsque le dessus de la pizza est doré).Faire cuire dans une poêle les lardons et les champignons.\n\nDans un bol, verser la boîte de concentré de tomate, y ajouter un demi verre d'eau, ensuite mettre un carré de sucre (pour enlever l'acidité de la tomate) une pincée de sel, de poivre, et une pincée d'herbe de Provence.\n\nDérouler la pâte à pizza sur le lèche frite de votre four, piquer-le.\n\nAvec une cuillère à soupe, étaler délicatement la sauce tomate, ensuite y ajouter les lardons et les champignons bien dorer. Parsemer de fromage râpée.\n\nMettre au four à 220°, thermostat 7-8, pendant 20 min (ou lorsque le dessus de la pizza est doré",
-              isFavorite: false,
-              favoriteCount: 50)),
+      home: RecipeListScreen(),
     );
   }
 }
